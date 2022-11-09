e569c3a0af26dfb0a78ed20822849b979743c27d
jisoooit <ydsn336@naver.com>
2022-11-09 13:36:40 +0900

    feat: view단 연결 controller추가

--- naverPay/src/main/java/org/example/shopping/controller/ShopDetailViewController.java
+++ naverPay/src/main/java/org/example/shopping/controller/ShopDetailViewController.java
@@ -1,10 +1,14 @@
 package org.example.shopping.controller;
 
+import jakarta.validation.constraints.Min;
 import org.example.sessions.SessionMgr;
+import org.example.shopping.dto.ShopListDetailDTO;
+import org.example.shopping.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,23 +18,39 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("shop-list")
 public class ShopDetailViewController {
     private SessionMgr sessionMgr; // = SessionMgr.getInstance();
+    private PaymentService paymentService;
 
     @Autowired
-    public ShopDetailViewController(SessionMgr sessionMgr) {
+    public ShopDetailViewController(SessionMgr sessionMgr, PaymentService paymentService) {
         this.sessionMgr = sessionMgr;
+        this.paymentService = paymentService;
     }
 
     @GetMapping("/detail")
     public String shopListDetail(Model model, HttpSession session, HttpServletRequest httpServletRequest) {
         String view = "shopDetail";
-        if (session.getAttribute("SESSION_ID") != null) { // 로그인이 되어있는 상태
+        if (session.getAttribute("SESSION_ID") == null) {
             return "redirect:/";
         }
 
-        sessionMgr.create(session,"imakecookieid");
         model.addAttribute("uId", sessionMgr.get(session));
 
         System.out.println(model.getAttribute("detail"));
         return view;
+    }
+
+    @GetMapping("detailpage/{pmId}")
+    public String getShopListDetail(@PathVariable @Min(1) Integer pmId, Model model) {
+        if (pmId <1) {
+            return "redirect:/"; //쇼핑리스트 페이지로 돌아가게 하면 될 것 같음
+        }
+        ShopListDetailDTO shopListDetailDTO = paymentService.getByPaymentId(pmId);
+
+        if (shopListDetailDTO == null) {
+            return "redirect:/"; //쇼핑리스트 페이지로 돌아가게 하면 될 것 같음
+        }
+
+        model.addAttribute("detail", shopListDetailDTO.toVO());
+        return "shopDetail";
     }
 }
